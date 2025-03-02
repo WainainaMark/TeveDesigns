1cc5bd9bc8db98b0e10bcfea9beb1249fdad2093
Steve Mark <105312132+WainainaMark@users.noreply.github.com>
2025-03-02 15:50:46 +0000

    tweaks

--- static/index.js
+++ static/index.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const section = document.getElementById('section')
+    const rectangle = document.getElementById('rectangle')
 
+    rectangle.style.opacity = "1"
     setTimeout(()=> {
         section.style.opacity = "0"
     }, 4000)
