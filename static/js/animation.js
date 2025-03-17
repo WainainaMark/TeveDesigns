28718dc5d81f2283b6ecc711691bfb97afeed531
Steve Mark <105312132+WainainaMark@users.noreply.github.com>
2025-03-17 07:02:13 +0000

    Display Grid

--- static/js/animation.js
+++ static/js/animation.js
@@ -1,6 +1,18 @@
-$('document').ready(function(){
-    setTimeout(()=>{
+$("document").ready(function () {
+  designs = ["Graphic", "Motion", "App", "UI/UX"];
+  $("#loader").fadeIn();
+  $("#loader").css("display", "flex");
+  $("#loading").css("display", "block");
+  setTimeout(() => {
+    $("#logo").css("transform", "translateY(0px)");
+  }, 150);
 
-        $('#logo').css('transform', 'translateY(0px)')
-    }, 150)
-})
+  setTimeout(() => {
+    $("#loading").fadeIn();
+  }, 800);
+  setTimeout(() => {
+    $("#loader").fadeOut();
+  }, 3000);
+
+  
+});
