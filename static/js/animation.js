234e4e9ad69772911be11cf7431c4c7e4044d78b
Steve Mark <105312132+WainainaMark@users.noreply.github.com>
2025-04-25 20:21:42 +0000

    BIG CHANGES

--- static/js/animation.js
+++ static/js/animation.js
@@ -1,24 +1,3 @@
-$("document").ready(function () {
-  $("#loader").fadeIn();
-  $('.box').css('transition', 'all 0.5s ease')
-  $("#loader").css("display", "flex");
-  $("#loading").css("display", "block");
-  setTimeout(() => {
-    $("#logo").css("transform", "translateY(0px)");
-  }, 150);
-
-  setTimeout(() => {
-    $("#loading").fadeIn();
-  }, 800);
-  setTimeout(() => {
-    $("#loader").fadeOut(function(){
-      $('#desktop').fadeIn()
-      $('#desktop').css('display', 'grid')
-      $('.box').css('top', '0px')
-    });
-  }, 3000);
-
-
-
-  
-});
+$(document).ready(function(){
+  console.log("Loaded")
+})
--- static/js/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const section = document.getElementById('section')
-    const rectangle = document.getElementById('rectangle')
-
-    rectangle.style.opacity = "1"
-    setTimeout(()=> {
-        section.style.opacity = "0"
-    }, 4000)
-})
